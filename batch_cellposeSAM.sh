8d344d017d3d9391eddc762f43be2b4f61831209
Adam Luff <aluff2@gmail.com>
2025-10-29 21:37:38 +1000

    Added drafts of cellpose counter wrappers and python script.

---
+++ batch_cellposeSAM.sh
@@ -0,0 +1,59 @@
+#!/bin/bash --login
+
+# Slurm directives for resource allocation
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --job-name=cellpose_counts
+#SBATCH --time=01:00:00
+#SBATCH --partition=gpu_cuda
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:l40:1
+#SBATCH --account=a_suarez
+#SBATCH -o /home/%u/Logs/output-%j.log
+#SBATCH -e /home/%u/Logs/error-%j.log
+
+# gres options include:
+# l40 48GB - gpu:l40:1
+# a100 10GB slice - gpu:nvidia_a100_80gb_pcie_1g.10gb:1
+# a100 20GB slice - gpu:nvidia_a100_80gb_pcie_2g.20gb:1
+
+# Load modules
+module load cuda/12.2.0
+module load cudnn/8.9.2.26-cuda-12.2.0
+module load miniforge/24.11.3-0
+
+# Parse options
+while getopts "d:" opt; do
+    case $opt in
+        d) DIAMETER=$OPTARG ;;
+        \?) echo "invalid option: -$OPTARG" >&2; exit 1 ;;
+    esac
+done
+
+# Prep conda
+#. /sw/auto/rocky8c/epyc3_l40/software/Miniconda3/4.12.0/etc/profile.d/conda.sh
+conda-activate
+conda activate /sw/local/rocky8/noarch/rcc/software/cellpose/4
+
+# Set scripts and directories
+PY_SCRIPT="$HOME/Software/HPC/HPC_atlas/Python/cellpose_counter.py"
+INPUT_DIR="/QRISdata/Q7597/processing/cellpose_in"
+OUTPUT_DIR="/QRISdata/Q7597/analysis"
+
+# Init arrays
+declare -a file_list
+
+# Build file list from input directory
+for file in $INPUT_DIR/*.tif; do 
+	file_list+=("$file")
+done
+
+# python $PY_SCRIPT ${file_list[@]} # DEPRECATED
+python3 -c 'import sys,' \
+           'sys.path.append("'(dirname $PY_SCRIPT)'"),' \
+           'import cellpose_counter,' \
+           'file_string = "'${file_list[@]}'",' \
+           'file_list = file_string.split(),' \
+           'cellpose_counter.cellposeSAM_count(file_list)'
---
+++ cellpose_counter.py
@@ -0,0 +1,101 @@
+import os
+import sys
+import csv
+import numpy as np
+from cellpose import models
+from cellpose.io import imread, masks_flows_to_seg, save_rois, save_masks
+
+################# Cellpose SAM #################
+
+def cellposeSAM_counter(input_files):
+    # Initialise cellpose model
+    model = models.CellposeModel(gpu = True, 
+                                 pretrained_model = 'cpsam')
+    images = [imread(f) for f in input_files]
+    normalize_params = {
+        "lowhigh" : None,
+        "normalize" : True,
+        "percentile" : [1.0,99.0],
+        }
+    # Alternative params to directly assign dynamic range
+    # normalize_params = {
+    #     "lowhigh" : [100,4000],
+    #     }
+
+    # Run cellpose model on file list
+    masks, flows, _ = model.eval(images,
+                                 batch_size = 64,
+                                 normalize = normalize_params)
+    num_masks = [len(np.unique(mask)) - 1 for mask in masks]
+    
+    # Assign save paths
+    basenames = [os.path.splitext(os.path.basename(f))[0] for f in input_files]
+    npy_names = [os.path.splitext(f)[0] + '.npy' for f in input_files]
+    png_names = [os.path.splitext(f)[0] + '.png' for f in input_files]
+    
+    # Write counts per file to csv
+    with open(outpath + '/cellposeSAM_counts.csv', 'w' ,newline = '') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['Image', 'Cell count'])
+        for filename, cell_count in zip(input_files, num_masks):
+            csvwriter.writerow([filename, cell_count])
+
+    # Save other output files
+    masks_flows_to_seg(images, masks, flows, npy_names, diams = 30)
+    save_masks(images, masks, flows, png_names, png = True)
+    for rois, filename in zip(masks, basenames):
+        save_rois(rois, filename)
+
+################# Cellpose 3 #################
+
+def cellpose3_counter(input_files, diameter = 30, outpath = None):
+    # Initialise cellpose model
+    model = models.CellposeModel(gpu = True, 
+                                 pretrained_model = 'cyto3', 
+                                 nchan = 2, 
+                                 diam_mean = diameter)
+    images = [imread(f) for f in input_files]
+    normalize_params = {
+        "lowhigh" : None,
+        "normalize" : True,
+        "percentile" : [1.0,99.0],
+        }
+    # Alternative params to directly assign dynamic range
+    # normalize_params = {
+    #     "lowhigh" : [100,4000],
+    #     }
+
+    # Run cellpose model on file list
+    masks, flows, _ = model.eval(images, 
+                                 diameter = diameter, 
+                                 channels = [0,0], 
+                                 flow_threshold = 0.4, 
+                                 do_3D = False, 
+                                 normalize = normalize_params)
+    num_masks = [len(np.unique(mask)) - 1 for mask in masks]
+
+    # Assign save paths names
+    basenames = [os.path.splitext(os.path.basename(f))[0] for f in input_files]
+    npy_names = [os.path.splitext(f)[0] + '.npy' for f in input_files]
+    png_names = [os.path.splitext(f)[0] + '.png' for f in input_files]
+    
+    # Write counts per file to csv
+    with open(outpath + '/cellpose3_counts.csv', 'w', newline = '') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['Image', 'Cell count'])
+        for filename, cell_count in zip(input_files, num_masks):
+            csvwriter.writerow([filename, cell_count])
+
+    # Write other output files
+    masks_flows_to_seg(images, masks, flows, npy_names, diams = diameter)
+    save_masks(images, masks, flows, png_names, png = True)
+    for rois, filename in zip(masks, basenames):
+        save_rois(rois, filename)
+
+if __name__=="__main__":
+    cell_diameter = int(sys.argv[1])
+    outpath = sys.argv[2]
+    input_files = sys.argv[3:]
+    
+    cellposeSAM_counter(input_files, outpath)
+    # cellpose3_counter(input_files, cell_diameter, outpath)
